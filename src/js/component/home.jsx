e85a3a7eb0f1eb6d0d10d245e788f23ca21e20fe
brandenreilly <155998782+brandenreilly@users.noreply.github.com>
2024-03-01 15:45:59 +0000

    added hover/focus modifiers + repeat functionality

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,14 +3,16 @@ import SongSelector from "./selector.jsx"
 
 const Home = () => {
 	const [isPlaying, setIsPlaying] = useState(false)
-	/* 
-	var myAudioObject = new Audio();
+
+	/* var myAudioObject = new Audio();
 	for (var key in myAudioObject){
    		if (typeof myAudioObject[key] === "number"){
        console.log(key);
    		}
-	}
-	 */
+		else if (typeof myAudioObject[key] === "function"){
+			console.log(key)
+		}
+	} */
 		var audioRef = null
 		var currentSong = 0;
 		const playAudio = (index) => {
@@ -35,6 +37,11 @@ const Home = () => {
 			console.log(audioRef.volume)
 			if(audioRef.volume <= 0.1){
 				alert("Volume can not go any lower.")
+			}
+		}
+		const repeatSong = () => {
+			if(audioRef.currentTime === audioRef.duration){
+				audioRef.play(currentSong)
 			}
 		}
 		var currentSong = 0;
@@ -72,7 +79,10 @@ const Home = () => {
 		
 			<footer className="text-center fixed-bottom">
 				<div className="MediaRow row d-flex justify-content-center">
-					<div className="col-8">
+					<div className="col-3">
+					<button className="btn MediaIcons" onClick={()=>{repeatSong()}}><i class="fas fa-redo fa-lg"></i></button>
+					</div>
+					<div className="col-5">
 						<button className="btn MediaIcons" onClick={()=>{playAudio(currentSong - 1)}}><i className="fas fa-backward fa-lg"></i></button>
 						<button className="btn MediaIcons" style={{display: isPlaying ? "none" : ""}} onClick={()=>{playAudio(currentSong)}}><i class="fas fa-play fa-lg"></i></button>
 						<button className="btn MediaIcons" style={{display: isPlaying ? "" : "none"}} onClick={()=>{pauseAudio()}}><i class="fas fa-pause-circle fa-lg"></i></button>
@@ -83,7 +93,7 @@ const Home = () => {
 						<button className="btn MediaIcons" onClick={()=>{raiseVolume()}}><i class="fas fa-volume-up fa-lg"></i></button>
 					</div>
 				</div>
-				<audio ref={(e)=>audioRef=e}></audio>
+				<audio controls ref={(e)=>audioRef=e}></audio>
 			</footer> 
 		</div>
 	);
--- src/js/component/selector.jsx
+++ src/js/component/selector.jsx
@@ -4,7 +4,7 @@ import { isPlaying } from "./home";
 const SongSelector = (props) => { 
     
     return (
-        <div onClick={()=>props.playAudio(props.index)} className="selector">
+        <div onClick={()=>{props.playAudio(props.index)}} tabIndex={0} className="selector">
             <p className="songList songPosition" ><strong>{props.index + 1}</strong></p>
             <p className="songList songName">{props.song.title} -</p>
         </div>
